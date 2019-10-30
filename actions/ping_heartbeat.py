7a67fb416364ec372f7735c7088048f81338de26
elan-wrussell <32270040+elan-wrussell@users.noreply.github.com>
2019-10-30 17:49:15 +0000

    Fix urllib moves

--- actions/ping_heartbeat.py
+++ actions/ping_heartbeat.py
@@ -11,7 +11,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-import urllib
+from six.moves.urllib.request import pathname2url
 
 from lib.actions import OpsGenieBaseAction
 
@@ -34,7 +34,7 @@ class PingHeartbeatAction(OpsGenieBaseAction):
         method = method
 
         data = self._req(method,
-                         "v2/heartbeats/" + urllib.pathname2url(name) + "/ping",
+                         "v2/heartbeats/" + pathname2url(name) + "/ping",
                          body=body)
 
         return data
