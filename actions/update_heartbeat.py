6c97a23b393b1945b73fa047f1ef3f55cd3a7572
elan-wrussell <32270040+elan-wrussell@users.noreply.github.com>
2019-10-30 17:49:55 +0000

    Fix urllib moves

--- actions/update_heartbeat.py
+++ actions/update_heartbeat.py
@@ -11,7 +11,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-import urllib
+from six.moves.urllib.request import pathname2url
 
 from lib.actions import OpsGenieBaseAction
 
@@ -68,7 +68,7 @@ class UpdateHeartbeatAction(OpsGenieBaseAction):
             body["alertPriority"] = alertPriority
 
         data = self._req("PATCH",
-                         "v2/heartbeats/" + urllib.pathname2url(name),
+                         "v2/heartbeats/" + pathname2url(name),
                          body=body)
 
         return data
